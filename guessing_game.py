4f7bbbac0e83962f1d74afe8e2b4724e1588112b
Aman Kumar Jha <amanj8547@gmail.com>
2026-09-25 23:30:02 +0530

    Improve guessing game with random number

--- guessing_game.py
+++ guessing_game.py
@@ -1,12 +1,19 @@
-secret_number = 7
+import random
+
+secret_number = random.randint(1, 10)
+attempts = 0
 
 while True:
     guess = int(input("Guess the number (1-10): "))
+    attempts += 1
 
     if guess == secret_number:
-        print("Correct! You guessed it.")
+        print("Correct!")
+        print("Attempts:", attempts)
         break
+
     elif guess < secret_number:
         print("Too low. Try again.")
+
     else:
         print("Too high. Try again.")
