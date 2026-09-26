72e73ff227b4257d5c2a491efadc8f0948f6b45b
Aman Kumar Jha <amanj8547@gmail.com>
2026-09-27 01:02:48 +0530

    Improve todo list with delete option

--- todo_list.py
+++ todo_list.py
@@ -3,7 +3,8 @@ tasks = []
 while True:
     print("\n1. Add task")
     print("2. Show tasks")
-    print("3. Exit")
+    print("3. Delete task")
+    print("4. Exit")
 
     choice = input("Enter your choice: ")
 
@@ -16,10 +17,25 @@ while True:
         if not tasks:
             print("No tasks yet.")
         else:
-            for task in tasks:
-                print("-", task)
+            for i, task in enumerate(tasks, start=1):
+                print(i, "-", task)
 
     elif choice == "3":
+        if not tasks:
+            print("No tasks to delete.")
+        else:
+            for i, task in enumerate(tasks, start=1):
+                print(i, "-", task)
+
+            number = int(input("Enter task number to delete: "))
+
+            if 1 <= number <= len(tasks):
+                tasks.pop(number - 1)
+                print("Task deleted.")
+            else:
+                print("Invalid task number.")
+
+    elif choice == "4":
         print("Goodbye!")
         break
 
